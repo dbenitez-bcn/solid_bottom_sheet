47ac6a52e4ea1615718f84e916910f9e40d6af2b
dbenitez-bcn <dbr.bcn@gmail.com>
2019-10-26 11:59:57 +0200

    Adds height and smoothness fields on solidController to improve the controll of the solid bottom sheet

--- lib/src/solidController.dart
+++ lib/src/solidController.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/foundation.dart';
 
+import '../solid_bottom_sheet.dart';
+
 class SolidController extends ValueNotifier<bool> {
+  // This is the current height of the bottomSheet's body
+  double _height;
+
+  // This is the current smoothness of the bottomSheet
+  Smoothness _smoothness;
+
   SolidController() : super(false);
 
   get isOpened => value;
+
+  double get height => _height;
+
+  set height(double value) {
+    _height = value;
+  }
+
+  Smoothness get smoothness => _smoothness;
+
+  set smoothness(Smoothness value) {
+    _smoothness = value;
+  }
 
   void hide() => value = false;
 
