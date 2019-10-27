7146bc5d43b2d13f90e5bd68c0f5b586fabea233
dbenitez-bcn <dbr.bcn@gmail.com>
2019-10-27 17:39:38 +0100

    Removed getters and setter from SolidController

--- lib/src/solidController.dart
+++ lib/src/solidController.dart
@@ -4,26 +4,14 @@ import '../solid_bottom_sheet.dart';
 
 class SolidController extends ValueNotifier<bool> {
   // This is the current height of the bottomSheet's body
-  double _height;
+  double height;
 
   // This is the current smoothness of the bottomSheet
-  Smoothness _smoothness;
+  Smoothness smoothness;
 
   SolidController() : super(false);
 
   get isOpened => value;
-
-  double get height => _height;
-
-  set height(double value) {
-    _height = value;
-  }
-
-  Smoothness get smoothness => _smoothness;
-
-  set smoothness(Smoothness value) {
-    _smoothness = value;
-  }
 
   void hide() => value = false;
 
